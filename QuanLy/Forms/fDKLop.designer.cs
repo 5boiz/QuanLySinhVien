a432414faae747afdc3eeaef150dd581f83d11a7
Vũ Ngọc Tuấn <tuandz1120@gmail.com>
2021-12-27 17:33:23 +0700

    Thêm form đăng ký Lớp

---
+++ QuanLy/Forms/fDKLop.designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace QuanLy.UC_control
+{
+    partial class fDKLop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNamHoc = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCapNhap = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.lblLop = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbKhoa = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTenLop = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbGiaoVien = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtNamHoc
+            // 
+            this.txtNamHoc.Location = new System.Drawing.Point(171, 350);
+            this.txtNamHoc.Name = "txtNamHoc";
+            this.txtNamHoc.Size = new System.Drawing.Size(254, 22);
+            this.txtNamHoc.TabIndex = 75;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(66, 350);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 23);
+            this.label4.TabIndex = 74;
+            this.label4.Text = "Năm học";
+            // 
+            // btnCapNhap
+            // 
+            this.btnCapNhap.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btnCapNhap.BackColor = System.Drawing.SystemColors.Control;
+            this.btnCapNhap.Location = new System.Drawing.Point(171, 400);
+            this.btnCapNhap.Name = "btnCapNhap";
+            this.btnCapNhap.Size = new System.Drawing.Size(105, 41);
+            this.btnCapNhap.TabIndex = 72;
+            this.btnCapNhap.Text = "Cập nhập";
+            this.btnCapNhap.UseVisualStyleBackColor = false;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btnThoat.BackColor = System.Drawing.SystemColors.Control;
+            this.btnThoat.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnThoat.Location = new System.Drawing.Point(320, 400);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(105, 41);
+            this.btnThoat.TabIndex = 73;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // lblLop
+            // 
+            this.lblLop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(150)))), ((int)(((byte)(250)))));
+            this.lblLop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblLop.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLop.Location = new System.Drawing.Point(0, 0);
+            this.lblLop.Name = "lblLop";
+            this.lblLop.Size = new System.Drawing.Size(494, 55);
+            this.lblLop.TabIndex = 71;
+            this.lblLop.Text = "Lớp";
+            this.lblLop.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(66, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 23);
+            this.label3.TabIndex = 70;
+            this.label3.Text = "Khoa";
+            // 
+            // cbKhoa
+            // 
+            this.cbKhoa.FormattingEnabled = true;
+            this.cbKhoa.Location = new System.Drawing.Point(171, 233);
+            this.cbKhoa.Name = "cbKhoa";
+            this.cbKhoa.Size = new System.Drawing.Size(254, 24);
+            this.cbKhoa.TabIndex = 69;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 23);
+            this.label2.TabIndex = 68;
+            this.label2.Text = "Tên lớp";
+            // 
+            // txtTenLop
+            // 
+            this.txtTenLop.Location = new System.Drawing.Point(171, 173);
+            this.txtTenLop.Name = "txtTenLop";
+            this.txtTenLop.Size = new System.Drawing.Size(254, 22);
+            this.txtTenLop.TabIndex = 67;
+            // 
+            // txtID
+            // 
+            this.txtID.Enabled = false;
+            this.txtID.Location = new System.Drawing.Point(171, 109);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(254, 22);
+            this.txtID.TabIndex = 66;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(66, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 23);
+            this.label1.TabIndex = 65;
+            this.label1.Text = "ID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(66, 295);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 23);
+            this.label5.TabIndex = 77;
+            this.label5.Text = "Giáo viên";
+            // 
+            // cbGiaoVien
+            // 
+            this.cbGiaoVien.FormattingEnabled = true;
+            this.cbGiaoVien.Location = new System.Drawing.Point(171, 294);
+            this.cbGiaoVien.Name = "cbGiaoVien";
+            this.cbGiaoVien.Size = new System.Drawing.Size(254, 24);
+            this.cbGiaoVien.TabIndex = 76;
+            // 
+            // fDKLop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(180)))), ((int)(((byte)(250)))));
+            this.ClientSize = new System.Drawing.Size(494, 492);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbGiaoVien);
+            this.Controls.Add(this.txtNamHoc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCapNhap);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.lblLop);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbKhoa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTenLop);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "fDKLop";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "fDKLop";
+            this.Load += new System.EventHandler(this.fDKLop_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNamHoc;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCapNhap;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label lblLop;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbKhoa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTenLop;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbGiaoVien;
+    }
+}
